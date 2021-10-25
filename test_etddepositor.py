3b7f12d9ac17968e6626b44aea30deffb34f7e6c
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2021-10-25 15:02:37 -0400

    Add more unit tests

--- test_etddepositor.py
+++ test_etddepositor.py
@@ -1,5 +1,6 @@
 import etddepositor
 import pytest
+import xml.etree.ElementTree as ElementTree
 
 
 def test_cwrite_metadata_csv_header(tmp_path):
@@ -105,3 +106,274 @@ def test_process_date():
         etddepositor.MetadataError, match="not properly formatted"
     ):
         etddepositor.process_date("BLAH")
+
+
+def test_process_subjects():
+    mappings = {
+        "lc_subject": {
+            "B001": [["a", "Agriculture."]],
+            "B013": [
+                ["a", "Wood."],
+                ["a", "Forest products.", "x", "Biotechnology"],
+            ],
+        }
+    }
+
+    subject_elements = [
+        ElementTree.Element("subject"),
+        ElementTree.Element("subject"),
+        ElementTree.Element("subject"),
+    ]
+
+    subject_elements[0].text = "  B001"
+    subject_elements[1].text = "B013  "
+    subject_elements[2].text = "Unknown"
+
+    assert etddepositor.process_subjects(subject_elements, mappings) == [
+        ["a", "Agriculture."],
+        ["a", "Wood."],
+        ["a", "Forest products.", "x", "Biotechnology"],
+    ]
+
+
+def test_process_description():
+    assert (
+        etddepositor.process_description("   \n\r   Abstract!\n  \n\r")
+        == "Abstract!"
+    )
+
+
+def test_process_contributors():
+    contributor_no_role = ElementTree.Element("contributor")
+    contributor_no_role.text = "Kevin Bowrin"
+    contributor_with_role = ElementTree.Element("contributor")
+    contributor_with_role.text = "James Ronin"
+    contributor_with_role.set("role", "co-author")
+
+    assert etddepositor.process_contributors(
+        [contributor_no_role, contributor_with_role]
+    ) == ["Kevin Bowrin", "James Ronin (Co-author)"]
+
+
+def test_process_language():
+    assert etddepositor.process_language("fre") == "fra"
+    assert etddepositor.process_language("fra") == "fra"
+    assert etddepositor.process_language("eng") == "eng"
+    assert etddepositor.process_language("") == "eng"
+    with pytest.raises(
+        etddepositor.MetadataError, match="unexpected language"
+    ):
+        etddepositor.process_language("bla")
+
+
+def test_process_degree():
+    assert etddepositor.process_degree("Master of Stuff") == "Master of Stuff"
+    assert (
+        etddepositor.process_degree(" Master of Stuff ") == "Master of Stuff"
+    )
+    assert (
+        etddepositor.process_degree("Master of Architectural Stud")
+        == "Master of Architectural Studies"
+    )
+    assert (
+        etddepositor.process_degree("Master of Information Tech")
+        == "Master of Information Technology"
+    )
+    assert etddepositor.process_degree("") == etddepositor.FLAG
+
+
+def test_process_degree_abbreviation():
+    mappings = {
+        "abbreviation": {
+            "Doctor of Philosophy": "Ph.D.",
+        }
+    }
+
+    assert (
+        etddepositor.process_degree_abbreviation(
+            "Doctor of Philosophy", mappings
+        )
+        == "Ph.D."
+    )
+
+    assert (
+        etddepositor.process_degree_abbreviation("Unknown", mappings)
+        == etddepositor.FLAG
+    )
+
+
+def test_process_degree_discipline():
+    mappings = {
+        "discipline": {
+            "MA-07": "Communication",
+            "MA-15": "English",
+        }
+    }
+
+    assert (
+        etddepositor.process_degree_discipline("MA-07", mappings)
+        == "Communication"
+    )
+
+    assert (
+        etddepositor.process_degree_discipline("   MA-15   ", mappings)
+        == "English"
+    )
+
+    assert (
+        etddepositor.process_degree_discipline("Unknown", mappings)
+        == etddepositor.FLAG
+    )
+
+
+def test_process_degree_level():
+    assert etddepositor.process_degree_level("1") == "1"
+    assert etddepositor.process_degree_level("2") == "2"
+    with pytest.raises(
+        etddepositor.MetadataError, match="degree level is missing"
+    ):
+        etddepositor.process_degree_level("")
+    with pytest.raises(etddepositor.MetadataError, match="undergraduate work"):
+        etddepositor.process_degree_level("0")
+    with pytest.raises(
+        etddepositor.MetadataError, match="invalid degree level"
+    ):
+        etddepositor.process_degree_level("blah")
+
+
+def test_create_package_data():
+    mappings = {
+        "abbreviation": {"Doctor of Philosophy": "Ph.D."},
+        "discipline": {"PHD-01": "Processing Studies"},
+        "lc_subject": {
+            "CODE1": [["a", "TestCode1."], ["a", "Test2", "x", "Specify"]],
+            "CODE2": [["a", "TestCode2."]],
+        },
+    }
+    package_metadata_xml = ElementTree.ElementTree(
+        ElementTree.fromstring(
+            """
+<thesis xmlns="http://www.ndltd.org/standards/metadata/etdms/1.1/"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns:dc="http://purl.org/dc/elements/1.1/"
+xmlns:dcterms="http://purl.org/dc/terms/"
+xsi:schemaLocation="http://www.ndltd.org/standards/metadata/etdms/1.1/
+http://www.ndltd.org/standards/metadata/etdms/1.1/etdms11.xsd
+http://purl.org/dc/elements/1.1/
+http://www.ndltd.org/standards/metadata/etdms/1.1/etdmsdc.xsd
+http://purl.org/dc/terms/
+http://www.ndltd.org/standards/metadata/etdms/1.1/etdmsdcterms.xsd"
+>
+  <dc:title xml:lang="en">Title</dc:title>
+  <dc:creator>Creator, Test</dc:creator>
+  <dc:subject>CODE1</dc:subject>
+  <dc:subject>CODE2</dc:subject>
+  <dc:description role="abstract" xml:lang="en">
+    \u00E9Abstract\n\r
+  </dc:description>
+  <dc:publisher>Publisher</dc:publisher>
+  <dc:contributor role="co-author">Contributor A</dc:contributor>
+  <dc:contributor>Contributor B</dc:contributor>
+  <dc:date>2021-01-01</dc:date>
+  <dc:type>Electronic Thesis or Dissertation</dc:type>
+  <dc:language>fre</dc:language>
+  <degree>
+    <name>Doctor of Philosophy</name>
+    <level>2</level>
+    <discipline>PHD-01</discipline>
+    <grantor>Carleton University</grantor>
+  </degree>
+</thesis>"""
+        )
+    )
+    assert etddepositor.create_package_data(
+        package_metadata_xml,
+        "StudentNumber_ThesisNumber",
+        77,
+        "/a/path/here",
+        mappings,
+    ) == etddepositor.PackageData(
+        name="StudentNumber_ThesisNumber",
+        source_identifier=(
+            "8fa99d4e9e189018f4781a5549d0f092"
+            "616664c2d15403c4a83b3d62b967719d"
+        ),
+        title="Title",
+        creator="Creator, Test",
+        subjects=[
+            ["a", "TestCode1."],
+            ["a", "Test2", "x", "Specify"],
+            ["a", "TestCode2."],
+        ],
+        abstract="\u00E9Abstract",
+        publisher="Publisher",
+        contributors=["Contributor A (Co-author)", "Contributor B"],
+        date="2021",
+        language="fra",
+        degree="Doctor of Philosophy",
+        abbreviation="Ph.D.",
+        discipline="Processing Studies",
+        level="2",
+        url="",
+        doi=f"{etddepositor.DOI_PREFIX}/etd/2021-77",
+        path="/a/path/here",
+    )
+
+    empty_package_metadata_xml = ElementTree.ElementTree(
+        ElementTree.fromstring(
+            """
+<thesis xmlns="http://www.ndltd.org/standards/metadata/etdms/1.1/"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns:dc="http://purl.org/dc/elements/1.1/"
+xmlns:dcterms="http://purl.org/dc/terms/"
+xsi:schemaLocation="http://www.ndltd.org/standards/metadata/etdms/1.1/
+http://www.ndltd.org/standards/metadata/etdms/1.1/etdms11.xsd
+http://purl.org/dc/elements/1.1/
+http://www.ndltd.org/standards/metadata/etdms/1.1/etdmsdc.xsd
+http://purl.org/dc/terms/
+http://www.ndltd.org/standards/metadata/etdms/1.1/etdmsdcterms.xsd"
+>
+</thesis>"""
+        )
+    )
+
+    with pytest.raises(
+        etddepositor.MetadataError, match="title tag is missing"
+    ):
+        etddepositor.create_package_data(
+            empty_package_metadata_xml,
+            "StudentNumber_ThesisNumber",
+            1,
+            "/a/path/here",
+            mappings,
+        )
+
+    empty_with_title_package_metadata_xml = ElementTree.ElementTree(
+        ElementTree.fromstring(
+            """
+    <thesis xmlns="http://www.ndltd.org/standards/metadata/etdms/1.1/"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:dcterms="http://purl.org/dc/terms/"
+    xsi:schemaLocation="http://www.ndltd.org/standards/metadata/etdms/1.1/
+    http://www.ndltd.org/standards/metadata/etdms/1.1/etdms11.xsd
+    http://purl.org/dc/elements/1.1/
+    http://www.ndltd.org/standards/metadata/etdms/1.1/etdmsdc.xsd
+    http://purl.org/dc/terms/
+    http://www.ndltd.org/standards/metadata/etdms/1.1/etdmsdcterms.xsd"
+    >
+      <dc:title xml:lang="en">Title</dc:title>
+    </thesis>"""
+        )
+    )
+
+    with pytest.raises(
+        etddepositor.MetadataError, match="creator tag is missing"
+    ):
+        etddepositor.create_package_data(
+            empty_with_title_package_metadata_xml,
+            "StudentNumber_ThesisNumber",
+            1,
+            "/a/path/here",
+            mappings,
+        )
